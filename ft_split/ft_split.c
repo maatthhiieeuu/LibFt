08b4d4f57f26c0f89ced919cd750ff9ffbbb9868
maatthhiieeuu <matt@DebWorkStation.ipv6.abo.wanadoo.fr>
2024-06-10 16:55:16 +0200

    écriture de ft_split.c sans test effectuer

--- ft_split/ft_split.c
+++ ft_split/ft_split.c
@@ -5,10 +5,83 @@
 /*   By: Matthieu Boegler                             +#:  +#::#+ :#+   +:#      #:+             */
 /*      <https://github.com/maatthhiieeuu>           ###   ##:## ###   #::::::::#                */
 /*                                                  ###    ###  ###   ###      ###               */
-/*   Created: 2024/06/09  by m.boegler             ###         ###   ###########                 */
-/*   Updated: 2024/06/09  by m.boegler            ###         ###   #########                    */
+/*   Created: 2024/06/  by m.boegler             ###         ###   ###########                 */
+/*   Updated: 2024/06/  by m.boegler            ###         ###   #########                    */
 /*                                                                                               */
 /* ********************************************************************************************* */
+
+#include <stdlib.h>
+//#include <>
+
+char	**ft_split(char const *s, char c)
+{
+	if(!s)
+		return(NULL);
+
+/******    Initialisation des variables    ******/
+	char	**result = NULL;
+	char	*str = (char *)s;
+	size_t	i = 0;
+	size_t	j = 0;
+	size_t	k = 0;
+	size_t	tab_len = 1;
+	size_t	str_len = 0;
+
+/******    Calcul du nombre d'adresse nécessaire pour le tableau result    ******/
+	while(str[i] != '\0')
+	{
+		if(str[i] != c)
+			tab_len++;
+		i++;
+	}
+
+/******    Initialisation du tableau result    ******/
+	result = (char **)malloc((tab_len) * sizeof(char *));
+	if(!result)
+		return(NULL);
+
+/******    Calcul de la taille et initialisations des pointeurs du tableau result    ******/
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if(str[i] != c)
+			str_len++;
+		else if(str[i] == c)
+		{
+			result[j] = (char *)malloc((str_len + 1) * sizeof(char));
+			if(!result[j])
+				return(NULL);
+			j++;
+			str_len = 0;
+		}
+		i++;
+	}
+	result[j] = NULL;
+
+/******    Ecriture des sous-chaînes    ******/
+	i = 0;
+	j = 0;
+	while(str[i] != '\0' && result[j] != NULL)
+	{
+		while(str[i] != c)
+		{
+			result[j][k] = str[i];
+			i++;
+			k++;
+			if(str[i] == c)
+				result[j][k] = '\0';
+		}
+		while(str[i] == c)
+		{
+			i++;
+		}
+		j++;
+		k = 0;
+	}
+
+/******    Retour de la fonction    ******/
+	return(result);	
+}
 
 /*
 
--- ft_split/main-ft_split.c
+++ ft_split/main-ft_split.c
@@ -9,3 +9,50 @@
 /*   Updated: 2024/06/09  by m.boegler            ###         ###   #########                    */
 /*                                                                                               */
 /* ********************************************************************************************* */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#define grey "\033[1;30m"
+#define red "\033[1;31m"
+#define green "\033[1;32m"
+#define orange "\033[1;33m"
+#define blue "\033[1;34m"
+#define d_green "\033[1;35m"
+#define purple "\033[1;36m"
+#define reset "\033[1;0m"
+
+char	**ft_split(char const *s, char c);
+
+int	main()
+{
+	char	*str = "1e2e3e4e5e6e7e8e9eAeBeCeDeEeFeGeHeIeJeKeLeMeNeOePeQeReSeTeUeVeWeXeYeZ";
+	char	set = 'e';
+	char	**result = NULL;
+	size_t	i = 0;
+	size_t	j = 0;
+
+	result = ft_split(str,set);
+
+	printf(orange"test 1 :\n"reset);
+	while(!result[i])
+	{
+		printf(blue"result[%ld] = |%s|\n"reset, i, result[i]);
+		i++;
+	}
+	printf(green"FIN DES TESTS.\n\n"reset);
+	j = 0;
+	while(result[j] != NULL)
+	{
+		j++;
+	}
+	free(result[j]);
+	j = 0; 
+	while(result[j])
+	{
+		free(result[j]);
+		j++;
+	}
+	free(result);
+}
