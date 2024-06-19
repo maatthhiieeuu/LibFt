8f43dcc27ff4ed283247f3cb4787c70eca995a37
maatthhiieeuu <matt@DebWorkStation.ipv6.abo.wanadoo.fr>
2024-06-19 12:16:28 +0200

    push ft_split.c et préparation de l'implémentation de ft_itoa.c

--- ft_split/main-ft_split.c
+++ ft_split/main-ft_split.c
@@ -5,54 +5,103 @@
 /*   By: Matthieu Boegler                             +#:  +#::#+ :#+   +:#      #:+             */
 /*      <https://github.com/maatthhiieeuu>           ###   ##:## ###   #::::::::#                */
 /*                                                  ###    ###  ###   ###      ###               */
-/*   Created: 2024/06/09  by m.boegler             ###         ###   ###########                 */
-/*   Updated: 2024/06/09  by m.boegler            ###         ###   #########                    */
+/*   Created: 2024/06/19  by m.boegler             ###         ###   ###########                 */
+/*   Updated: 2024/06/19  by m.boegler            ###         ###   #########                    */
 /*                                                                                               */
 /* ********************************************************************************************* */
 
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define grey "\033[1;30m"
-#define red "\033[1;31m"
-#define green "\033[1;32m"
-#define orange "\033[1;33m"
-#define blue "\033[1;34m"
-#define d_green "\033[1;35m"
-#define purple "\033[1;36m"
-#define reset "\033[1;0m"
+char **ft_split(char const *s, char c);
 
-char	**ft_split(char const *s, char c);
-
-int	main()
-{
-	char	*str = "1e2e3e4e5e6e7e8e9eAeBeCeDeEeFeGeHeIeJeKeLeMeNeOePeQeReSeTeUeVeWeXeYeZ";
-	char	set = 'e';
-	char	**result = NULL;
-	size_t	i = 0;
-	size_t	j = 0;
-
-	result = ft_split(str,set);
-
-	printf(orange"test 1 :\n"reset);
-	while(!result[i])
-	{
-		printf(blue"result[%ld] = |%s|\n"reset, i, result[i]);
-		i++;
-	}
-	printf(green"FIN DES TESTS.\n\n"reset);
-	j = 0;
-	while(result[j] != NULL)
-	{
-		j++;
-	}
-	free(result[j]);
-	j = 0; 
-	while(result[j])
-	{
-		free(result[j]);
-		j++;
-	}
-	free(result);
+// Fonction pour libérer la mémoire allouée par ft_split
+void free_split(char **split) {
+    if (!split) return;
+    for (size_t i = 0; split[i] != NULL; i++) {
+        free(split[i]);
+    }
+    free(split);
 }
+
+// Fonction pour comparer les résultats et afficher le succès ou l'échec
+void compare_results(char **result, char **expected, const char *test_name) {
+    int success = 1;
+    size_t i = 0;
+
+    while (expected[i] != NULL) {
+        if (result[i] == NULL || strcmp(result[i], expected[i]) != 0) {
+            success = 0;
+            break;
+        }
+        i++;
+    }
+
+    if (result[i] != NULL) {
+        success = 0;
+    }
+
+    if (success) {
+        printf("\033[1;32mSUCCESS:\033[0m %s\n", test_name);
+        printf("  \033[1;30mExpected:\033[0m\n");
+        for (i = 0; expected[i] != NULL; i++) {
+            printf("    \033[1;30m'%s'\033[0m\n", expected[i]);
+        }
+        printf("  \033[1;30mGot:\033[0m\n");
+        for (i = 0; result[i] != NULL; i++) {
+            printf("    \033[1;30m'%s'\033[0m\n", result[i]);
+        }
+    } else {
+        printf("\033[1;31mFAILED:\033[0m %s\n", test_name);
+        printf("  \033[1;30mExpected:\033[0m\n");
+        for (i = 0; expected[i] != NULL; i++) {
+            printf("    \033[1;30m'%s'\033[0m\n", expected[i]);
+        }
+        printf("  \033[1;30mGot:\033[0m\n");
+        for (i = 0; result[i] != NULL; i++) {
+            printf("    \033[1;30m'%s'\033[0m\n", result[i]);
+        }
+    }
+}
+
+// Fonction pour exécuter un test
+void run_test(const char *str, char delim, char **expected, const char *test_name) {
+    char **result = ft_split(str, delim);
+    compare_results(result, expected, test_name);
+    free_split(result);
+}
+
+int main() {
+    // Cas de test 1 : chaîne avec un seul délimiteur
+    char *expected1[] = {"j'aime le ", "ylophone", NULL};
+    run_test("j'aime le xylophone", 'x', expected1, "Test 1: chaîne avec un seul délimiteur");
+
+    // Cas de test 2 : chaîne sans délimiteur
+    char *expected2[] = {"il n'y a pas de délimiteur ici", NULL};
+    run_test("il n'y a pas de délimiteur ici", 'z', expected2, "Test 2: chaîne sans délimiteur");
+
+    // Cas de test 3 : chaîne avec plusieurs délimiteurs
+    char *expected3[] = {"abc", "def", "ghi", NULL};
+    run_test("abc,def,ghi", ',', expected3, "Test 3: chaîne avec plusieurs délimiteurs");
+
+    // Cas de test 4 : délimiteur au début et à la fin de la chaîne
+    char *expected4[] = {"bonjour", "monde", NULL};
+    run_test(",bonjour,monde,", ',', expected4, "Test 4: délimiteur au début et à la fin de la chaîne");
+
+    // Cas de test 5 : chaîne vide
+    char *expected5[] = {NULL};
+    run_test("", 'x', expected5, "Test 5: chaîne vide");
+
+    // Cas de test 6 : chaîne avec des délimiteurs consécutifs
+    char *expected6[] = {"a", "b", "c", "d", NULL};
+    run_test("a;;b;c;;d", ';', expected6, "Test 6: chaîne avec des délimiteurs consécutifs");
+
+    //Cas de test 7 : chaîne avec beaucoup de délimiteurs
+    char *expected7[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", NULL};
+    run_test("1e2e3e4e5e6e7e8e9eAeBeCeDeEeFeGeHeIeJeKeLeMeNeOePeQeReSeTeUeVeWeXeYeZ", 'e', expected7, "Test 7: chaîne avec beaucoup de délimiteurs");
+
+		
+    return 0;
+}
+
---
+++ libft/ft_split.c
@@ -0,0 +1,187 @@
+/* ********************************************************************************************* */
+/*                                                                                               */
+/*   Project : libft/ft_split.c                         ::::::  ::::::    :::::::::              */
+/*                                                     +:: ::+ +:: ::+   +:::::::::+             */
+/*   By: Matthieu Boegler                             +#:  +#::#+ :#+   +:#      #:+             */
+/*      <https://github.com/maatthhiieeuu>           ###   ##:## ###   #::::::::#                */
+/*                                                  ###    ###  ###   ###      ###               */
+/*   Created: 2024/06/19  by m.boegler             ###         ###   ###########                 */
+/*   Updated: 2024/06/19  by m.boegler            ###         ###   #########                    */
+/*                                                                                               */
+/* ********************************************************************************************* */
+#include <stdlib.h>
+//#include "libft.h"
+
+size_t	count_adress_necessary(char const *str, char c);
+char  **allocation_result(size_t tab_len);
+int    account_allocates_sentence_length(char *str, char c, char **result);
+void	initialization_substrings(char *str, char **result, char c, size_t tab_len);
+void	free_result(char **result, size_t tab_len);
+
+char	**ft_split(char const *s, char c)
+{
+	if(!s)
+		return(NULL);
+
+	char	**result	= NULL;
+	char	*str 		= (char *)s;
+	size_t	tab_len		= 0;
+
+	tab_len = count_adress_necessary(str, c);
+	result 	= allocation_result(tab_len);
+	if(!result)
+		return(NULL);
+	if(!account_allocates_sentence_length(str, c, result))
+	{
+		free_result(result, tab_len);
+		return(NULL);
+	}
+	initialization_substrings(str, result, c, tab_len);
+
+	return(result);	
+}
+
+/******    Calcul du nombre d'adresse nécessaire pour le tableau result    ******/
+size_t	count_adress_necessary(char const *str, char c)
+{
+	if(!str)
+		return(0);
+
+	size_t			tab_len = 0;
+	size_t			i 		= 0;
+	unsigned int	flag 	= 0;
+
+	while(str[i] != '\0')
+	{
+		if(str[i] != c && flag == 0)
+			flag = 1;
+		if((str[i] == c || str[i + 1] == '\0') && flag == 1)
+		{
+			tab_len++;
+			flag = 0;
+		}
+		i++;
+	}
+
+	return(tab_len);
+}
+
+/******    Initialisation du tableau result    ******/
+char	**allocation_result(size_t tab_len)
+{
+	char	**result	= NULL;
+	
+	result = (char **)malloc((tab_len + 1) * sizeof(char *));
+	if(!result)
+		return(NULL);
+	
+	return(result);
+}
+
+/******    Calcul de la taille des string déclaration et initialisations des pointeurs du tableau result    ******/
+int    account_allocates_sentence_length(char *str, char c, char **result)
+{
+	if(!str || !result)
+		return(0);
+
+	size_t	str_len	= 0;  
+	size_t	i		= 0;
+	size_t	j		= 0;
+	unsigned int 	flag = 0;
+
+	while(str[i] != '\0')
+	{
+		if(str[i] != c)
+		{
+			str_len++;
+			flag = 1;
+		}
+		if((str[i] == c || str[i + 1] == '\0') && flag == 1)
+		{
+			result[j] = (char *)malloc((str_len + 1) * sizeof(char));
+			if(!result)
+				return(0);
+			str_len = 0;
+			j++;
+			flag = 0;
+		}
+		i++;
+	}
+		if(str[i] == '\0')
+			result[j] = NULL;
+
+	return(1);
+}
+
+/******    Ecriture des sous-chaînes    ******/
+void	initialization_substrings(char *str, char **result, char c, size_t tab_len)
+{
+	if(!str || !result)
+		return;
+
+	size_t			i		= 0;
+	size_t			j		= 0;
+	size_t			k		= 0;
+	unsigned int	flag	= 0;
+
+	while(str[i] != '\0')
+	{
+		if(str[i] != c)
+		{
+			if(result[j] == NULL)
+			{
+				free_result(result, tab_len);
+				return;
+			}
+			result[j][k] = str[i];
+			k++;
+			flag = 1;
+		}
+		else if(str[i] == c && flag == 1)
+		{
+			result[j][k] = '\0';
+			j++;
+			k = 0;
+			flag = 0;
+		}
+		i++;
+	}
+	if(j < tab_len && result[j] != NULL)
+		result[j][k] = '\0';
+}
+
+void	free_result(char **result, size_t tab_len)
+{
+	size_t	i	= 0;
+
+	while(i < tab_len)
+	{
+		if(result[i])
+			free(result[i]);
+		i++;
+	}
+	free(result);
+}
+
+/*
+
+Function name:		ft_split
+
+Prototype:			char **ft_split(char const *s, char c);
+
+Paramètres:			s: La chaîne de caractères à découper.
+					c: Le caractère délimiteur.
+
+Valeur de retour:	Le tableau de nouvelles chaînes de caractères
+					résultant du découpage.
+					NULL si l’allocation échoue.
+Fonctions
+ ex-
+ternes autorisées:	malloc, free
+
+Description:		Alloue (avec malloc(3)) et retourne un tableau
+					de chaînes de caractères obtenu en séparant ’s’ à
+					l’aide du caractère ’c’, utilisé comme délimiteur.
+					Le tableau doit être terminé par NULL.
+
+*/
